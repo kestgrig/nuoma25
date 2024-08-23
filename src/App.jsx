6bbfafba6f3a2f259ae4a2d807d656d24e37a026
kestgrig <kestutis.grigenas@gmail.com>
2024-08-23 15:54:53 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import style from'./App.module.css';
-import { Header } from './components/header/Header';
 import { LoginSection } from './components/loginSection/LoginSection'
-import netflixBackGround from '../img/netflix-background.png'
 
 function App() {
 return (
     <>
-      <div className={style.headerLogInSection} style={{
-      backgroundImage: `url(${netflixBackGround} `, 
-      height:'900px', 
-      width:'100%', 
-      backgroundRepeat:'no-repeat',
-
-      }} >
-        <Header />
         <LoginSection />
-      </div>
     </>
   );
 }
--- src/components/loginSection/LoginSection.jsx
+++ src/components/loginSection/LoginSection.jsx
@@ -1,18 +1,34 @@
 import style from '../loginSection/LoginSection.module.css'
+import logo from '/img/netflix-logo.png';
+
 
 export function LoginSection () {
 
   return (
     <>
-      <div className={style.content}>
-        <h1>Unlimited movies, TV <br/> shows, and more</h1>
-        <h2>Starts at EUR 7.99. Cancel anytime.</h2>
-        <p>Ready to watch? Enter your email to create or restart your membership.</p>
-        <div className={style.emailForm}>
-          <input type="email" placeholder="Email address" className={style.emailInput}/>
-          <button className={style.getStartedButton}>Get Started</button>
+      <div className={style.backGround}>
+        <header className={style.mainHeader}>
+          <div className={style.mainlogo}>
+            <img className={style.logo} src={logo} alt="Logo" />
+          </div>
+          <nav className={style.btnSelect}>
+            <select className={style.languageSelect}>
+              <option value="english">English</option>
+              <option value="lithuanian">Lietuvių</option>
+            </select>
+            <button className={style.signInBtn}>Sign In</button>
+          </nav>
+        </header>
+        <section className={style.content}>
+          <h1 className={style.unlimited}>Unlimited movies,<br/> TV  shows, and more</h1>
+          <h2 className={style.price}>Starts at EUR 7.99. Cancel anytime.</h2>
+          <p className={style.enterEmail}>Ready to watch? Enter your email to create or restart your membership.</p>
+          <div className={style.emailForm}>
+            <input type="email" placeholder="Email address" className={style.emailInput}/>
+            <button className={style.getStartedButton}>Get Started</button>
+          </div>
+        </section>
         </div>
-      </div>
     </>
   );
 }

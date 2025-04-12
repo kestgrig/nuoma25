8bd679437afcfe4ae4b51c82be7f3140c0a268f7
kestgrig <kestutis.grigenas@gmail.com>
2025-04-12 04:40:39 +0300

    dsds

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kestgrig/nuoma25/'
+  base: 'https://github.com/kestgrig/nuoma25'
 })
